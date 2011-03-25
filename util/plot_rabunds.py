4ec12d6a5aa8ed781f3d836eeb8acd8232a90386
Maksim Sipos <msipos@mailc.net>
2011-03-25 15:09:29 -0500

    plot_rabunds updated to do binning if it's available

--- util/plot_rabunds.py
+++ util/plot_rabunds.py
@@ -2,8 +2,21 @@
 
 import pylab, numpy, scipy, sys
 
+have_maxpy = False
+try:
+    import maxpy
+    have_maxpy = True
+except ImportError, e:
+    pass
+
 for f in sys.argv[1:]:
     d = numpy.loadtxt(f)
-    pylab.semilogy(d, label=f)
+
+    if have_maxpy:
+        dx = numpy.array(range(1, len(d)+1))
+        x, y, e = maxpy.smart_bin(dx, d, 40)
+        pylab.semilogy(x, y, '-x', label=f)
+    else:
+        pylab.semilogy(d, label=f)
 pylab.legend(loc='upper right')
 pylab.show()
